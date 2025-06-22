06e90d52af561c5f8f04be6d116d89983d4bd3c8
Adityasharma0709 <7rockyrocky@gmail.com>
2025-06-22 16:42:33 +0530

    topBar update again

--- Silicon_proj_Fullstack/src/app/shared/topbar/topbar.component.ts
+++ Silicon_proj_Fullstack/src/app/shared/topbar/topbar.component.ts
@@ -7,6 +7,18 @@ import { Component, Input } from '@angular/core';
 })
 export class TopbarComponent {
   @Input() teamRoute: string = 'team1'; // default to team1
-  
+  activeIndex = 0;
+
+
+  navItems = [
+    { iconClass: 'fas fa-home', route: 'home' },
+    { iconClass: 'fas fa-heart', route: 'about' },
+    { iconClass: 'fa-solid fa-photo-film', route: 'gallery' },
+    { iconClass: 'fa-solid fa-envelopes-bulk', route: 'contact' },
+  ];
+
+  setActive(index: number) {
+    this.activeIndex = index;
+  }
 
 }
