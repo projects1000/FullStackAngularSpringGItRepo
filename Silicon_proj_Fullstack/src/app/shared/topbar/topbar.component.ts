f63f719951683d994c2c49ab72e6f13ebcdfbefc
Aditya Sharma <138314085+Adityasharma0709@users.noreply.github.com>
2025-06-22 16:22:06 +0530

    Update topbar.component.ts

--- Silicon_proj_Fullstack/src/app/shared/topbar/topbar.component.ts
+++ Silicon_proj_Fullstack/src/app/shared/topbar/topbar.component.ts
@@ -1,12 +1,1 @@
-import { Component, Input } from '@angular/core';
 
-@Component({
-  selector: 'app-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
-})
-export class TopbarComponent {
-  @Input() teamRoute: string = 'team1'; // default to team1
-  
-
-}
