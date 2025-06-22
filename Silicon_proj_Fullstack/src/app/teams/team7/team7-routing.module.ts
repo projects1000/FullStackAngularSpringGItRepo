cf45ed3f0d07c6f72ae83b362dcbb660e60dcb79
Arpita <arpitapatnaik1367@gmail.com>
2025-06-22 22:28:47 +0530

    team 7 lead changes

--- Silicon_proj_Fullstack/src/app/teams/team7/team7-routing.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team7/team7-routing.module.ts
@@ -2,8 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Team7Component } from './team7.component';
 
-const routes: Routes = [{ path: '', component: Team7Component }];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
+const routes: Routes = [
+  {
+      path: '',
+      component: Team7Component,
+      children: [
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', component: HomeComponent },
+        { path: 'about', component: AboutComponent },
+        { path: 'gallery', component: GalleryComponent },
+        { path: 'contact', component: ContactComponent },
+      ]
+    }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
