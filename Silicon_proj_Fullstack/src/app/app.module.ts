24ca5528ef13613acf8a7214c0f6821c40e8a498
Arpita <arpitapatnaik1367@gmail.com>
2025-06-22 10:00:51 +0530

    done

--- Silicon_proj_Fullstack/src/app/app.module.ts
+++ Silicon_proj_Fullstack/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+// import { HomeComponent } from './team7/pages/home/home.component';
+// import { AboutComponent } from './team7/pages/about/about.component';
+// import { GalleryComponent } from './team7/pages/gallery/gallery.component';
+// import { ContactComponent } from './team7/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
+    // HomeComponent,
+    // AboutComponent,
+    // GalleryComponent,
+    // ContactComponent,
   ],
   imports: [
     BrowserModule,
--- Silicon_proj_Fullstack/src/app/teams/team7/team7.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team7/team7.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { Team7RoutingModule } from './team7-routing.module';
 import { Team7Component } from './team7.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    Team7Component
+    Team7Component,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
