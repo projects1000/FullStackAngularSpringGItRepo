c2016a76ffcff220f8dd95628b60ed3de8878290
Adityasharma0709 <7rockyrocky@gmail.com>
2025-06-22 20:27:03 +0530

    homePage

--- Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+array:any=
+  {
+    src:'../assets/Screenshot 2025-06-22 183447.png'
+  }
 
 }
