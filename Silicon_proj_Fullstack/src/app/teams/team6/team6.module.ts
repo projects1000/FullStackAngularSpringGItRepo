69f3db0e176c21a51c57fd6bf7c70224219d0735
= <mohantarohan84@gmail.com>
2025-06-23 21:49:14 +0530

    updated

--- Silicon_proj_Fullstack/src/app/teams/team6/team6.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team6/team6.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { Team6RoutingModule } from './team6-routing.module';
 import { Team6Component } from './team6.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 @NgModule({
   declarations: [
-    Team6Component
+    Team6Component,
+    PagesComponent,
+    HomeComponent,
+    GalleryComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
