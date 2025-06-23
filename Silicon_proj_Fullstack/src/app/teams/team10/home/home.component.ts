e6b194447d5c8b2eae6528c63ff6a3ee60fd022a
Adityasharma0709 <7rockyrocky@gmail.com>
2025-06-23 09:07:52 +0530

    badge add

--- Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
@@ -5,9 +5,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-array:any=
-  {
-    src:'../assets/Screenshot 2025-06-22 183447.png'
-  }
+  selected: string | null = null;
+  fadingOut: string | null = null;
+  timeoutId: any = null;
 
+  members = [
+    {
+      name: 'Pratyush',
+      img: '../../../../assets/team10/assets/Memeber1.jpg',
+      github: 'https://github.com/pratyush-git',
+      key: 'pratyush'
+    },
+    {
+      name: 'Aditya',
+      img: '../../../../assets/team10/assets/Screenshot183431.png',
+      github: 'https://github.com/aditya-git',
+      key: 'aditya'
+    },
+    {
+      name: 'Trisha',
+      img: '../../../../assets/team10/assets/Member3.jpg',
+      github: 'https://github.com/trisha-git',
+      key: 'trisha'
+    },
+    {
+      name: 'P.Priyadrashi',
+      img: '../../../../assets/team10/assets/Screenshot2025.png',
+      github: 'https://github.com/priyadrashi-git',
+      key: 'priyadrashi'
+    },
+    {
+      name: 'Swetalina',
+      img: '../../../../assets/team10/assets/Memeber4.jpg',
+      github: 'https://github.com/swetalina-git',
+      key: 'swetalina'
+    }
+  ];
+
+  showGitHub(key: string) {
+    this.selected = key;
+    this.fadingOut = null;
+
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+
+    this.timeoutId = setTimeout(() => {
+      this.fadingOut = key;
+
+      setTimeout(() => {
+        this.selected = null;
+        this.fadingOut = null;
+        this.timeoutId = null;
+      }, 500); // Delay for fade-out duration
+    }, 1500); // Microcard shown for 3 seconds
+  }
 }
