e7e3ed850f1d11266bfb76e72d8bd48867fcc35f
Adityasharma0709 <7rockyrocky@gmail.com>
2025-06-23 09:18:02 +0530

    github link add

--- Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team10/home/home.component.ts
@@ -13,32 +13,37 @@ export class HomeComponent {
     {
       name: 'Pratyush',
       img: '../../../../assets/team10/assets/Memeber1.jpg',
-      github: 'https://github.com/pratyush-git',
+      github: 'https://github.com/pratyushkumar-04',
       key: 'pratyush'
+      ,username:'pratyushkumar-04'
     },
     {
       name: 'Aditya',
       img: '../../../../assets/team10/assets/Screenshot183431.png',
-      github: 'https://github.com/aditya-git',
+      github: 'https://github.com/Adityasharma0709',
       key: 'aditya'
+      ,username:'Adityasharma0709'
     },
     {
       name: 'Trisha',
       img: '../../../../assets/team10/assets/Member3.jpg',
-      github: 'https://github.com/trisha-git',
+      github: 'https://github.com/Trisha123',
       key: 'trisha'
+      ,username:'Trisha123'
     },
     {
       name: 'P.Priyadrashi',
       img: '../../../../assets/team10/assets/Screenshot2025.png',
-      github: 'https://github.com/priyadrashi-git',
+      github: 'https://github.com/pratyush2004-vamp',
       key: 'priyadrashi'
+      ,username:'pratyush2004-vamp'
     },
     {
       name: 'Swetalina',
       img: '../../../../assets/team10/assets/Memeber4.jpg',
-      github: 'https://github.com/swetalina-git',
+      github: 'https://github.com/Swetalina-cyber',
       key: 'swetalina'
+      ,username:'Swetalina-cyber'
     }
   ];
 
