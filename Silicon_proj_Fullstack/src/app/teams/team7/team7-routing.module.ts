9b103a1e18a03a4af1296ee60ffc93ce0f709da2
Arpita <arpitapatnaik1367@gmail.com>
2025-06-23 09:31:20 +0530

    team 7 starts

--- Silicon_proj_Fullstack/src/app/teams/team7/team7-routing.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team7/team7-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Team7Component } from './team7.component';
-
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
