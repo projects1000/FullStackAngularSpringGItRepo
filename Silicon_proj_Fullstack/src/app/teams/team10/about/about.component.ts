78a8b453c932da3bc7e253ae6a55634a7db86129
Adityasharma0709 <7rockyrocky@gmail.com>
2025-06-23 16:51:01 +0530

    updated contact url in about

--- Silicon_proj_Fullstack/src/app/teams/team10/about/about.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team10/about/about.component.ts
@@ -3,15 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
   port: string = window.location.port;
   hostname: string = window.location.hostname;
-  contactUrl: string =` http://${this.hostname}:${this.port}/team10/contact`;
-
-
-  constructor() {
-    this.contactUrl = `http://${this.hostname}:${this.port}/team10/contact`;
-  }
+  contactUrl: string = `http://${this.hostname}:${this.port}/team10/contact`;
 }
