f1538407f18081a5faa21097f6b9184b9b287f79
nurag-pradhan <pradhananurag2005@gmail.com>
2025-06-23 22:23:55 +0530

    bug fixed in the code of team6.

--- Silicon_proj_Fullstack/src/app/teams/team6/team6.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team6/team6.module.ts
@@ -3,17 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { Team6RoutingModule } from './team6-routing.module';
 import { Team6Component } from './team6.component';
-import { PagesComponent } from './pages/pages.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SharedModule } from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
     Team6Component,
-    PagesComponent,
     HomeComponent,
     GalleryComponent,
     ContactComponent,
@@ -21,7 +20,8 @@ import { AboutComponent } from './pages/about/about.component';
   ],
   imports: [
     CommonModule,
-    Team6RoutingModule
-  ]
+    Team6RoutingModule,
+    SharedModule
+]
 })
 export class Team6Module { }
