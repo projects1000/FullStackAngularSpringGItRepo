c1dee2fa1f32496a8436bc6e5d649f53dc46ea27
nurag-pradhan <pradhananurag2005@gmail.com>
2025-06-23 15:34:40 +0530

    Added the home page of team 3.

--- Silicon_proj_Fullstack/src/app/teams/team3/pages/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team3/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements AfterViewInit {
 
+  ngAfterViewInit() {
+    const text = 'Team 3: Builders of Tomorrow 🚀';
+    const target = document.querySelector('.typewriter') as HTMLElement;
+    let index = 0;
+
+    if (target) {
+      const interval = setInterval(() => {
+        target.textContent += text.charAt(index);
+        index++;
+        if (index === text.length) {
+          clearInterval(interval);
+        }
+      }, 80); // speed of typing
+    }
+  }
 }
