d3df1f58da9f5b02e7812623a943aa05e3674d81
Smruti <smrutirani2003@gmail.com>
2025-06-24 17:30:26 +0530

    home component

--- Silicon_proj_Fullstack/src/app/teams/team6/pages/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team6/pages/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+constructor() { }
 
+  onStartLearning() {
+    // Add navigation logic or modal
+    console.log('Start Learning clicked');
+  }
+
+  onViewCurriculum() {
+    // Add navigation logic or modal
+    console.log('View Curriculum clicked');
+  }
 }
