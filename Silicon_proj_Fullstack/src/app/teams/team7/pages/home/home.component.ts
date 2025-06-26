6b45fd7a237aad7abd5d9a13489c5ec371dae393
JyotiKarmakar-20 <karmakarjyoti70@gmail.com>
2025-06-26 21:14:38 +0530

    Added Team7 home page

--- Silicon_proj_Fullstack/src/app/teams/team7/pages/home/home.component.ts
+++ Silicon_proj_Fullstack/src/app/teams/team7/pages/home/home.component.ts
@@ -1,22 +1,52 @@
+// import { Component } from '@angular/core';
+// import { DataService } from 'src/app/data.service';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent{
+
+//   users: any[] = [];
+//   dataLoaded = false;
+
+//   constructor(private dataService: DataService) {}
+
+//   loadUsers(): void {
+//     this.dataService.getUsers().subscribe(data => {
+//       this.users = data;
+//       this.dataLoaded = true;
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent{
+export class HomeComponent {
+  teamName: string = 'Team 7 - Innovation Division';
 
-  users: any[] = [];
-  dataLoaded = false;
+  teamLead = {
+    name: 'Arpita',
+    //image: 'https://randomuser.me/api/portraits/women/44.jpg' 
+    image: 'https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80'
 
-  constructor(private dataService: DataService) {}
+  };
 
-  loadUsers(): void {
-    this.dataService.getUsers().subscribe(data => {
-      this.users = data;
-      this.dataLoaded = true;
-    });
-  }
+  mission: string = 'We are passionate about innovation and technology. Our goal is to build impactful digital solutions that solve real-world problems with creativity and collaboration.';
+
+  teamMembers = [
+    { name: 'Arpita', role: 'Frontend Developer' },
+    { name: 'Jyoti Karmakar', role: 'Backend Developer' },
+    { name: 'Shobha Mahato', role: 'UI/UX Designer' },
+    { name: 'Soma Tiwary', role: 'DevOps Engineer' },
+    { name: 'Sajeya Quadri', role: 'Quality Analyst' },
+    { name: 'Akansha', role: 'Quality Analyst' },
+    { name: 'Ipsita P.Mishra', role: 'Quality Analyst' }
+  ];
 }
