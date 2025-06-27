372071cb9f8460dc3f845e3232e49b15a91901bf
= <pratyushpriyadarshi2004@gmail.com>
2025-06-27 14:44:17 +0530

    Pratyush Gallery Added

--- Silicon_proj_Fullstack/src/app/teams/team10/team10-routing.module.ts
+++ Silicon_proj_Fullstack/src/app/teams/team10/team10-routing.module.ts
@@ -4,6 +4,7 @@ import { Team10Component } from './team10.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 const routes: Routes = [{path: '', component: Team10Component, 
@@ -11,7 +12,9 @@ const routes: Routes = [{path: '', component: Team10Component,
         { path: '', redirectTo: 'home', pathMatch: 'full' },
        { path: 'home', component: HomeComponent },
        { path: 'about', component: AboutComponent },
-      {path:'contact',component:ContactComponent}]
+      {path:'contact',component:ContactComponent},
+      { path: 'gallery', component: GalleryComponent }]
+
 }];
 
 @NgModule({
